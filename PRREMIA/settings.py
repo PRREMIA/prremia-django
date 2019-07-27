e0bf8cd323bd74c188a16462f607f461d878f67a
Jinali Desai <prremia@gmail.com>
2019-07-27 14:57:52 +0530

    base_dir

--- PRREMIA/settings.py
+++ PRREMIA/settings.py
@@ -121,12 +121,11 @@ USE_TZ = True
 
 MEDIA_URL = '/media/'
 
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
